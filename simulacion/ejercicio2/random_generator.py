28be20cf1e3c15a8c567eab3a7b27fd013f80414
Francisco Ruiz <fruiz.rc@gmail.com>
2021-06-25 17:26:29 -0400

    fixed mixed congruential method typo and random method

--- simulacion/ejercicio2/random_generator.py
+++ simulacion/ejercicio2/random_generator.py
@@ -1,24 +1,31 @@
-class MixedCongruentialMixed():
+class MixedCongruentialMethod():
     def __init__(self, a, c, x, m):
         self._a = a
         self._x = x
         self._c = c
         self._m = m
 
-    def random(self, start=1, final=0):
+    def random(self, length=1, low=0, high=10):
         results = []
 
         # This range could be with start and final range or just N numbers
 
-        for i in (range(start, final) if final != 0 else range(start)):
-            pseudo_result = self._a * self._x + self._c
-            mod = pseudo_result % self._m
-            result = mod / self._m
+        for i in range(length):
+            aux = True
+            while(aux):
+
+                pseudo_result = self._a * self._x + self._c
+                mod = pseudo_result % self._m
+                result = mod / self._m
+
+                self._x = mod
+                if result > low/10 and result <= high/10:
+                    aux = False
+
             results.append(result)
-            self._x = mod
         return results
 
 
 if __name__ == '__main__':
-    random_generator = MixedCongruentialMixed(5, 7, 4, 8)
+    random_generator = MixedCongruentialMethod(5, 7, 4, 8)
     print(random_generator.random(2, 10))
