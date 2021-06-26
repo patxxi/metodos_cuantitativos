f858742fe6d8422e1644a27becdbe23138096e0b
Francisco Ruiz <fruiz.rc@gmail.com>
2021-06-26 15:15:02 -0400

    ready the 4 exercise, fuck it muñoz, fuck yourself dude

--- simulacion/ejercicio4/suscriptions.py
+++ simulacion/ejercicio4/suscriptions.py
@@ -2,33 +2,72 @@ import random
 
 
 def run():
-    number_clients = int(
-        input("Insert how many potential clients you will visit"))
-    random_number = random.random(1, 10)
-    if random_number > 7:
+    n = int(input("Write how many house will visite the seller: "))
+    articles_woman = {
+        "0.6": 1,
+        "0.3": 2,
+        "0.1": 3,
+        "0": 4
+    }
+
+    articles_man = {
+        "0.1": 1,
+        "0.4": 2,
+        "0.3": 3,
+        "0.2": 4
+    }
+    benefit = 0
+
+    for i in range(n):
+        random_number = random.randint(1, 100)
+
+        print("\n\n")
         print(
-            f"Numero aleatorio: {random_number}, si atendio alguien a la puerta")
-        random_number = random.random(1, 10)
-        if random_number <= 8:
-            print(f"Numero aleatorio: {random_number}, atendio un hombre")
-            random_number = random.random(1, 10)
+            f"---------------------------------------------------------CLIENTE NUMERO {i+1}---------------------------------------------------------")
+        if random_number >= 70:
+            print(
+                f"Numero aleatorio {random_number}, si fue recibido el vendedor")
+            random_number = random.randint(1, 100)
 
-            if random_number <= 2.5:
+            if random_number <= 80:
                 print(
-                    f"Numero aleatorio: {random_number}, el cliente si comprara al menos una suscripcion")
-                random_number = random.random(1, 10)
+                    f"numero aleatorio {random_number}, lo recibio un hombre")
 
+                random_number = random.randint(1, 100)
+
+                if random_number <= 25:
+                    print(
+                        f"Numero aleatorio {random_number}, se consiguio la venta")
+                    random_number = round(random.random(), 2)
+                    print(
+                        f"Numero aleatorio {random_number}, el hombre comprara un total de {articles_man[str(random_number)]} suscripciones")
+                    benefit += articles_man[str(random_number)] * 3000
+                else:
+                    print(
+                        f"Numero aleatorio {random_number}, no se consiguio la venta")
 
             else:
                 print(
-                    f"Numero aleatorio: {random_number}, el cliente no comprara ninguna suscripcion")
+                    f"numero aleatorio {random_number}, lo recibio una mujer")
+
+                random_number = random.randint(1, 100)
+                if random_number <= 15:
+
+                    print(
+                        f"Numero aleatorio {random_number}, se consiguio la venta")
+                    random_number = round(random.random(), 2)
+
+                    print(
+                        f"Numero aleatorio{random_number}, la mujer compara un total de {articles_woman[str(random_number)]} suscripciones")
+
+                    benefit += articles_woman[str(random_number)] * 3000
+                else:
+                    print(
+                        f"Numero aleatorio {random_number}, no se consiguio la venta")
 
         else:
-            print(f"Numero aleatorio: {random_number}, atendio una mujer")
-
-    else:
-        print(
-            f"Numero aleatorio: {random_number}, No atendio alguien a la puerta")
+            print(
+                f"Numero aleatorio {random_number}, no fue recibido el vendedor")
 
 
 if __name__ == '__main__':
