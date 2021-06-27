a550b7b5f71de91f667b952badc910d4f91e595f
Francisco Ruiz <fruiz.rc@gmail.com>
2021-06-27 18:33:29 -0400

    finish the exercise for queue theory

---
+++ simulacion/teoria_colas/exercise1.py
@@ -0,0 +1,89 @@
+from random_generator import MixedCongruentialMethod
+import math
+import random
+
+
+class QueueingTheory():
+
+    def __init__(self, miu, lamb):
+        self._lamb = lamb
+        self._miu = miu
+        # Tiempo medio de un paquete permanece en el sistema
+        self._w = 1 / (self._miu - self._lamb)
+        # Tiempo medio de espera en la cola
+        self._wq = self._w - (1 / self._miu)
+        # Intensidad del trafico en el sistema
+        self._roh = self._lamb / self._miu
+        # Numero medio de paquetes en el sistema
+        self._Ls = self._lamb * self._w
+        # Numero medio de paquetes en la cola
+        self._Lq = self._lamb * self._wq
+        # Probabilidad de que no existan paquetes en el sistema
+        self._p0 = (self._roh ** 2) / (1 - self._roh)
+
+    def probability_packages_on_system(self, N):
+        pn = (1 - (self._lamb / self._miu)) * (self._lamb / self._miu) ** N
+        return pn
+
+    def probability_income_package(self, N, time):
+        return math.e ** (-self._lamb*time) * ((self._lamb*time)**N)/math.factorial(N)
+
+    def probability_stay_time(self, time):
+        return (math.e ** (-self._miu * (1-self._roh) * time))
+
+
+def run():
+    queue = QueueingTheory(60/60, 45/60)
+    queue_probabilities = [
+        queue.probability_income_package(i, 1) for i in range(10)]
+    probabilities = []
+    PERCENTAGE = 100
+    clients = 0
+    actual_clients = 0
+    treated_clients = 0
+    PROBABILITY_BEING_SERVED = queue.probability_stay_time(1)
+    TIME = 60
+
+    for i in range(len(queue_probabilities)-1, 0, -1):
+        PERCENTAGE -= queue_probabilities[i] * 100
+        probabilities.append(PERCENTAGE)
+
+    probabilities.sort()
+
+    print(f"Tiempo promedio de espera en la cola: {queue._wq}")
+    print(f"Tiempo promedio en el sistema: {queue._w}")
+    print(f"Numero de clientes en la cola: {queue._Lq}")
+    print(f"Numero medio de paquetes en el sistema: {queue._Ls}")
+
+    for i in range(TIME):
+        print(
+            f"\n------------------------------------- Minuto {i+1} -------------------------------------")
+        random_number = random.random() * 100
+
+        prev = float()
+        for i, prob in enumerate(probabilities):
+            print(f"prev = {prev}, actual = {prob}")
+            if random_number <= prob and random_number > prev:
+                clients += i
+                actual_clients += i
+                print(f"Numero aleatorio generado: {random_number}")
+                print(f"Han entrado {i} paquetes a la cola")
+
+                break
+            prev = prob
+
+        random_number = random.random()
+        if random_number <= PROBABILITY_BEING_SERVED:
+            print("Se ha atendido a un cliente este minuto")
+            treated_clients += 1
+            actual_clients = (actual_clients - 1 if actual_clients > 0 else 0)
+
+        print(
+            f"Paquetes totales recibidos hasta este minuto: {clients}")
+        print(
+            f"Paquetes actuales en la cola en este minuto: {actual_clients}")
+        print(f"Clientes atendidos hasta este minuto: {treated_clients}")
+
+
+if __name__ == "__main__":
+    run()
